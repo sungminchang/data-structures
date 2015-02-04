d19da598f85287576c435b8ee987934b51bbc831
sungminchang <4sungmin@gmail.com>
2015-02-04 14:50:31 -0800

    Finish funcitonal patter queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var tail = 0;
+  var head = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[tail++] = value;
   };
 
   someInstance.dequeue = function(){
+    var result = storage[head];
+    delete storage[head];
+    (tail - head) && head++;
+
+    return result;
   };
 
-  someInstance.size = function(){
+  someInstance.tail = function(){
+    return tail - head;
   };
 
   return someInstance;
