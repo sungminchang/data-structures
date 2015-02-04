6e98eec861cf880e0ffccb24bec5039e8b512590
sungminchang <4sungmin@gmail.com>
2015-02-04 15:07:39 -0800

    Finish functional sharing stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newObject = {};
+  newObject.length = 0;
+
+  _.extend(newObject, stackMethods);
+  return newObject;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(val) {
+  this[this.length++] = val;
+};
 
+stackMethods.pop = function() {
+  if (this.length > 0) {
+    var result = this[--this.length];
+    delete this[this.length];
+    return result;
+  }
+};
+
+stackMethods.size = function() {
+  return this.length;
+};
