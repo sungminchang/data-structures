2c1ce92eab1b62d3a6b712ac35f5203f5c4b332e
sungminchang <4sungmin@gmail.com>
2015-02-04 16:09:42 -0800

    Finished pseudoclassical (minus one odd test) Stack.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
 
+Stack.prototype.push = function(val) {
+  this[this.length++] = val;
+};
 
+Stack.prototype.pop = function() {
+  if (this.length > 0) {
+    var result = this[--this.length];
+    delete this[this.length];
+    return result;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
+
+// var newObject = new Stack();
