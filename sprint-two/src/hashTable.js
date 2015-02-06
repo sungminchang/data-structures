b47d44ac7ed13ab60150da5a6481ef0a65e81e3f
sungminchang <4sungmin@gmail.com>
2015-02-06 12:11:44 -0800

    Finished Hash Table, should change insert() to check if key already exists

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,17 +3,45 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+// to do: insert() should check if the key already exists
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+
+  if (Array.isArray(bucket)) {
+    bucket.push([k, v]);
+  } else {
+    var temp = [];
+    var tuple = [k,v];
+    temp.push(tuple);
+    this._storage.set(i, temp);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
 
+  if (Array.isArray(bucket)) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
 
+  if (Array.isArray(bucket)) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = null;
+      }
+    }
+  }
 };
 
 
